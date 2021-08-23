3cc5253171822ada5193632f2ea80b431c013262
aryaaak <aryarizky2303@gmail.com>
2021-08-23 12:54:52 +0700

    Setup Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Bangkit Bersama App.</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/pages/MakeHelp/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MakeHelp = () => {
+    return (
+        <View>
+            <Text>Make Help Page.</Text>
+        </View>
+    )
+}
+
+export default MakeHelp
+
+const styles = StyleSheet.create({})
