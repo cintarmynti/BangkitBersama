e91791b4bf046b495e27e9395af15f6a147005a2
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2021-09-20 19:44:33 +0700

    commit sementara ini udah pake selectbox

--- src/containers/pages/HelpInput/index.js
+++ src/containers/pages/HelpInput/index.js
@@ -1,14 +1,69 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+import React, {useState} from 'react'
+import { View, Text, ScrollView, Image, TextInput, Modal } from 'react-native'
+import {P, InputText} from './../../../components'
+import {Colors} from './../../../utils'
 
 const HelpInput = () => {
+    const [pilihan, setPilihan] = useState();
+    const klipPilihan = (label) => {
+        alert("kamu pilih hari "+ label);
+        setPilihan(label);
+    }
     return (
-        <View>
-            <Text>Help Input Page.</Text>
-        </View>
+    <ScrollView style={{backgroundColor:Colors.background}}>
+        <View style={{padding:30}}>
+            {/* arrow  */}
+            <Image source={require('./../../../assets/icon/arrow-left.png')}/>
+
+            {/* button nav  */}
+            <View style={{flexDirection:'row', alignSelf:'center'}}>
+                <View>
+                    <View style={{width:35, height:35, borderRadius:35, backgroundColor:Colors.primary}}>
+                        <P style={{color:'white', textAlign:'center', justifySelf:'center', marginTop:5}} title="1"/>
+                    </View>
+                    <P title="form"/>
+                </View>
+                
+                <View style={{width:32, height:3, backgroundColor:Colors.primary, top:17, marginHorizontal:8}}></View>
+                <View>
+                    <View style={{width:35, height:35, borderRadius:35, backgroundColor:'white'}}>
+                        <P style={{color:Colors.primary, textAlign:'center', justifySelf:'center', marginTop:5}} title="2"/>
+                    </View>
+                    <P title="Proses" />
+                </View> 
+            </View>
+           
+
+                {/* form  */}
+                <View>
+                    {/* Selectbox  */}
+                    <View>
+                        <P title="Kategori" style={{marginTop:48, color:Colors.darkGrey}} />
+                        <Picker
+                            style={{backgroundColor:'white', marginTop:10, height:70 }}
+                            selectedValue={pilihan}
+                            onValueChange={(label, index) => klipPilihan(label)}
+                        >
+                            <Picker.Item label="Kategori bantuan" enabled={false}/>
+                            <Picker.Item label="Selasa" value="Selasa"/>
+                            <Picker.Item label="Rabu" value="Rabu"/>
+                            <Picker.Item label="Kamis" value="Kamis"/>
+
+                        </Picker>
+                    </View>
+
+                    {/* judul input text */}
+                    <View style={{marginTop:24}}> 
+                        <InputText placeholder="Judul Bantuan" name="Judul" />
+                    </View>
+                    
+                    {/* Foto input file */}
+                </View>
+            </View>
+    </ScrollView>
+        
     )
 }
 
 export default HelpInput
-
-const styles = StyleSheet.create({})
