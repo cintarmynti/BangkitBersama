458e981c533796ecc8355839ad89b532f3f08655
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2021-09-20 21:02:22 +0700

    styling

--- src/containers/pages/HelpInput/index.js
+++ src/containers/pages/HelpInput/index.js
@@ -1,7 +1,7 @@
 import { Picker } from '@react-native-picker/picker'
 import React, {useState} from 'react'
-import { View, Text, ScrollView, Image, TextInput, Modal } from 'react-native'
-import {P, InputText} from './../../../components'
+import { View, Text, ScrollView, Image, TextInput, Modal, StyleSheet } from 'react-native'
+import {P, InputText, PrimaryButton} from './../../../components'
 import {Colors} from './../../../utils'
 
 const HelpInput = () => {
@@ -35,35 +35,80 @@ const HelpInput = () => {
             </View>
            
 
-                {/* form  */}
+            {/* form  */}
+            <View style={{marginTop:48}}>
+                {/* Selectbox  */}
                 <View>
-                    {/* Selectbox  */}
-                    <View>
-                        <P title="Kategori" style={{marginTop:48, color:Colors.darkGrey}} />
-                        <Picker
-                            style={{backgroundColor:'white', marginTop:10, height:70 }}
-                            selectedValue={pilihan}
-                            onValueChange={(label, index) => klipPilihan(label)}
-                        >
-                            <Picker.Item label="Kategori bantuan" enabled={false}/>
-                            <Picker.Item label="Selasa" value="Selasa"/>
-                            <Picker.Item label="Rabu" value="Rabu"/>
-                            <Picker.Item label="Kamis" value="Kamis"/>
+                    <P title="Kategori" style={{color:Colors.darkGrey}} />
+                    <Picker
+                        style={{backgroundColor:'white', marginTop:10, height:70 }}
+                        selectedValue={pilihan}
+                        onValueChange={(label, index) => klipPilihan(label)}
+                    >
+                        <Picker.Item label="Kategori bantuan" enabled={false}/>
+                        <Picker.Item label="Selasa" value="Selasa"/>
+                        <Picker.Item label="Rabu" value="Rabu"/>
+                        <Picker.Item label="Kamis" value="Kamis"/>
 
-                        </Picker>
-                    </View>
-
-                    {/* judul input text */}
-                    <View style={{marginTop:24}}> 
-                        <InputText placeholder="Judul Bantuan" name="Judul" />
-                    </View>
-                    
-                    {/* Foto input file */}
+                    </Picker>
                 </View>
+
+                {/* judul input text */}
+                <View style={{marginTop:24}}> 
+                    <InputText placeholder="Judul Bantuan" name="Judul" />
+                </View>
+                
+                {/* Foto input file */}
+                <View style={{marginTop:24}}>
+                    <P title="Foto" style={{color:Colors.darkGrey}} />
+                    <View style={styles.inputPhoto}>
+                        <Image style={{alignSelf:'center'}} source={require('../../../assets/icon/img.png')} />
+                    </View>
+                </View>
+
+                {/* input deskripsi */}
+                <View style={{marginTop:24}}>
+                    <P title="Deskripsi" style={{color:Colors.darkGrey}} />
+                    <TextInput placeholder="Ceritakan tentang bantuan ini" style={styles.InputDesc}/>
+                </View>
+                
+                {/* jumlah penerima  */}
+                <View style={{marginTop:24}}> 
+                    <InputText name="Jumlah Penerima" placeholder="Berapa banyak penerima.." />
+                </View>
+
+                {/* tanggal  */}
+                <View style={{marginTop:24}}>
+                    <InputText name="Tanggal" placeholder="Masukkan tanggal" /> 
+                </View>
+                
+                <PrimaryButton title="Tawarkan" style={{marginTop:40, marginBottom:32, height:59}} />
             </View>
+        </View>
     </ScrollView>
         
     )
 }
 
 export default HelpInput
+
+
+const styles = StyleSheet.create({
+    inputPhoto: {
+        height:151, 
+        backgroundColor:'white', 
+        marginTop:10, 
+        borderRadius:15, 
+        justifyContent:'center',
+        borderWidth:1, 
+        borderStyle:'dashed', 
+        borderColor:Colors.grey
+    },
+    InputDesc:{
+        height:151, 
+        backgroundColor:'white', 
+        marginTop:10, borderRadius:15, 
+        paddingHorizontal:20
+    }
+   
+})
